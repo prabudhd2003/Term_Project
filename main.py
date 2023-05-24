38739730f5e248ac533d35f7af8281d8348e9845
Prabudhd Krishna Kandpal <96969241+prabudhd2003@users.noreply.github.com>
2023-05-24 17:55:18 +0530

    Update main.py
    Added  4 more stocks and their predictions and also updated the ARIMA model.

--- main.py
+++ main.py
@@ -3,61 +3,78 @@ import streamlit as st
 import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
-import pickle
 
-start_date = "1996-01-01"
-end_date = "2023-05-01"
+
 st.title("Reliance NSE Closing Stock Price Prediction")
-
+st.title("Stock Price Prediction")
 model_name = st.sidebar.selectbox("Select Model for prediction", ("ARIMA", "Facebook Prophet", "Stacked LSTM"))
+data_set = st.selectbox("Select Stock for prediction", ("RELIANCE", "TATA", "SBI", "ICICI", "ADANI"))
 
-@st.cache_data
 def get_dataset():
-    # data = yf.download('RELIANCE.NS', period='max', auto_adjust=True)
-    data = yf.download('RELIANCE.NS', start="1996-01-01", end="2023-05-01", auto_adjust=True)
-    # Split data into training and testing sets
-    size = int(len(data.Close) * 0.9)
-    train_data = list(data[0:size]['Close'])
-    test_data = list(data[size:]['Close'])
-    return data, train_data, test_data
+        if data_set=="RELIANCE":
+            data = yf.download('RELIANCE.NS', period='max', auto_adjust=True)
+            st.subheader('RELIANCE Raw Data')
+            st.write(data.tail())
+            st.subheader("Closing Price vs Time chart for entire RELIANCE dataset")
+            st.line_chart(data.Close, use_container_width=True)
+        elif data_set=="TATA":
+            data = yf.download('TATASTLLP.NS', period='max', auto_adjust=True)
+            st.subheader('TATA STEEL Raw Data')
+            st.write(data.tail())
+            st.subheader("Closing Price vs Time chart for entire TATA STEEL dataset")
+            st.line_chart(data.Close, use_container_width=True)
+        elif data_set=="SBI":
+            data = yf.download('SBIN.NS', period='max', auto_adjust=True)
+            st.subheader('SBI Raw Data')
+            st.write(data.tail())
+            st.subheader("Closing Price vs Time chart for entire SBI dataset")
+            st.line_chart(data.Close, use_container_width=True)
+        elif data_set=="ICICI":
+            data = yf.download('ICICIBANK.NS', period='max', auto_adjust=True)
+            st.subheader('ICICI Raw Data')
+            st.write(data.tail())
+            st.subheader("Closing Price vs Time chart for entire ICICI dataset")
+            st.line_chart(data.Close, use_container_width=True)
+        elif data_set=="ADANI":
+            data = yf.download('ADANIENT.NS', period='max', auto_adjust=True)
+            st.subheader('ADANI Raw Data')
+            st.write(data.tail())
+            st.subheader("Closing Price vs Time chart for entire ADANI dataset")
+            st.line_chart(data.Close, use_container_width=True)
+        # Split data into training and testing sets
+        size = int(len(data.Close) * 0.9)
+        train_data = list(data[0:size]['Close'])
+        test_data = list(data[size:]['Close'])
+        return data, train_data, test_data
 
 data, train_data, test_data = get_dataset()
 
-st.subheader('Raw data')
-st.write(data.tail())
-
-st.subheader("Closing Price vs Time chart for entire dataset")
-st.line_chart(data.Close, use_container_width=True)
-
-# st.subheader("Closing Price vs Time chart with 100MA")
 if model_name=="ARIMA":
     st.subheader(model_name)
-    # Loading ARIMA model
-    # with open('arima_model.pkl', 'rb') as f:
-    #    arima_model = pickle.load(f)
-    # Loading ARIMA predictions
-    predictions = pd.read_csv("./arima_predictions.csv")
-
-    st.subheader("ARIMA Predictions Mapping Testing Set")
+    if data_set == "RELIANCE":
+        predictions = pd.read_csv("./arima_predictions_reliance.csv")
+    elif data_set == "TATA":
+        predictions = pd.read_csv("./arima_predictions_tata.csv")
+    elif data_set == "SBI":
+        predictions = pd.read_csv("./arima_predictions_sbi.csv")
+    elif data_set == "ICICI":
+        predictions = pd.read_csv("./arima_predictions_icici.csv")
+    elif data_set == "ADANI":
+        predictions = pd.read_csv("./arima_predictions_adani.csv")
+    st.subheader("ARIMA predictions mapping Actual Dataset")
     fig2 = plt.figure(figsize=(20,8), dpi=300)
     date_range = data[int(len(data.Close) * 0.9):].index
     plt.plot(date_range, predictions["actual_data"], color='blue', marker='.', label='Actual')
     plt.plot(date_range, predictions["predictions"], color='red', marker='.', linestyle='--', label='Predictions')
-    plt.title("Reliance NSE Closing Stock Price Since 1996")
+    # plt.title("Reliance NSE Closing Stock Price Since 1996")
     plt.xlabel("Date")
     plt.ylabel("Closing Prices (Rs)")
     plt.grid()
     plt.legend()
     st.pyplot(fig2)
-    st.text("RMSE: 41.57")
-    
-    st.subheader("Interactive Plot")
-    predictions['Date'] = pd.to_datetime(predictions['Date'])
-    # Set the 'Date' column as the index
-    predictions = predictions.set_index('Date')
-    st.line_chart(predictions, use_container_width=True)
-    # st.line_chart(data=predictions, y=["actual_data", "predictions"])
-    
+    rmse_1 = np.sqrt(np.square(np.subtract(predictions["actual_data"], predictions["predictions"])).mean()).round(2)
+    st.write("RMSE: ", rmse_1)
+
     days = st.slider(label="Select days", value=14)
     st.subheader(f"ARIMA predictions for last {days} days")
     fig3 = plt.figure(figsize=(20,8), dpi=300)
@@ -72,6 +89,11 @@ if model_name=="ARIMA":
     st.pyplot(fig3)
     rmse = np.sqrt(np.square(np.subtract(predictions["actual_data"][-days:], predictions["predictions"][-days:])).mean()).round(2)
     st.write("RMSE: ", rmse)
+
+    predictions['Date'] = pd.to_datetime(predictions['Date'])
+    # Set the 'Date' column as the index
+    predictions = predictions.set_index('Date')
+    st.line_chart(predictions[["actual_data", "predictions"]], use_container_width=True)
 
 elif model_name=="Facebook Prophet":
     st.text("Prophet")
